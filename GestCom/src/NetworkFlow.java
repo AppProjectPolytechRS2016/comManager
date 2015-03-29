ab1448a7a7a7a6d088f1a9fab53aefcd94e07135
Wolfsimjo <sloeb-jeje@hotmail.fr>
2015-03-29 11:42:55 +0200

    Modification de NetworkFlow pour ajouter caractere de fin de chaine

--- GestCom/src/NetworkFlow.java
+++ GestCom/src/NetworkFlow.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
@@ -7,25 +6,22 @@ import java.io.IOException;
 public class NetworkFlow
 {
 	/**Methode permettant de lire le flux reseau d'entree et de convertir ce qui a ete lue en String*/
-	public static String readMessage(DataInputStream in) throws IOException,EOFException
+	/*public static String readMessage(DataInputStream in) throws IOException,EOFException
 	{
 		int taille = in.readInt();		
 		byte message[]=new byte[taille];
 		int nb = in.read(message,0,taille);
 		return new String(message);
-	}
+	}*/
 	
 	/**Methode permettant d'ecrire un message sur le flux reseau de sortie */
 	public static void writeMessage(DataOutputStream out, String s) throws IOException
 	{
-		//byte message[] = s.getBytes();		
-		//out.writeInt(message.length);
-		//out.write(message);
 		System.out.println("too");
-		out.writeChars(s);
+		out.writeChars(s+"\r\n");
 	}
 	
-	public static String readMessageBis(BufferedReader in) throws IOException,EOFException
+	public static String readMessage(BufferedReader in) throws IOException,EOFException
 	{
 		return in.readLine();
 	}
